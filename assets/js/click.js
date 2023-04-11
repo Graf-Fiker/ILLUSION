7f7c0e62a29feca0911cb3052177915b3dbaac75
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2023-04-11 13:52:07 +0200

    Delete assets directory

--- assets/js/click.js
+++
@@ -1,6 +0,0 @@
-const myButton = document.getElementById('myButton');
-
-myButton.addEventListener('click', function() {
-  // Toggle the "clicked" class when the button is clicked
-  myButton.classList.toggle('clicked');
-});
