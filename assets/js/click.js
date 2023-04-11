6fc113c59cfeccfbf17344e57cab645063e756f3
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2023-04-11 13:54:39 +0200

    Add files via upload

---
+++ assets/js/click.js
@@ -0,0 +1,6 @@
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', function() {
+  // Toggle the "clicked" class when the button is clicked
+  myButton.classList.toggle('clicked');
+});
