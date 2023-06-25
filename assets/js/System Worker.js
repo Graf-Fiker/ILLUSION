baca67441f4f65efd946a3029ad214fcab404772
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2023-06-25 18:07:07 +0200

    Update System Worker.js

--- assets/js/System Worker.js
+++ assets/js/System Worker.js
@@ -23,3 +23,62 @@ function refreshPage() {
 
 // Schedule the page refresh every 15 minutes
 setInterval(refreshPage, 15 * 60 * 1000);
+
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/image.jpg'
+  // Add more URLs of static assets you want to cache
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
+      .catch(error => {
+        console.error('Error in fetch event:', error);
+      })
+  );
+});
