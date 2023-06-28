a5e58a80d21ae05a50b51bb4a1890dbb079c0dc6
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2023-06-28 22:27:51 +0200

    Update pull.js

--- assets/js/pull.js
+++ assets/js/pull.js
@@ -1,24 +1,20 @@
-// Store the current page information when the pull-down gesture is detected
-function handlePullToRefresh() {
-  // Capture the current page information (e.g., URL)
-  const currentPage = window.location.href;
-  
-  // Save the current page information to localStorage
-  localStorage.setItem('currentPage', currentPage);
+// Check if page state is stored in local storage
+const storedPage = localStorage.getItem('activePage');
+const initialPage = storedPage || 'page1';
+
+// Set initial active tab
+activateTab(initialPage);
+
+// Add event listeners to navbar buttons
+for (let i = 0; i < navButtons.length; i++) {
+  navButtons[i].addEventListener('click', function() {
+    const tabId = this.getAttribute('data-tab');
+    activateTab(tabId);
+    // Store active page in local storage
+    localStorage.setItem('activePage', tabId);
+  });
 }
 
-// Retrieve the stored page information and navigate to the page
-function restorePage() {
-  // Retrieve the stored page information from localStorage
-  const storedPage = localStorage.getItem('currentPage');
+// ...
 
-  // Check if the stored page information exists
-  if (storedPage) {
-    // Redirect or navigate to the stored page
-    window.location.href = storedPage;
-  }
-}
-
-// Attach event listeners to handle pull-to-refresh gesture and page load
-window.addEventListener('touchstart', handlePullToRefresh);
-window.addEventListener('load', restorePage);
+// Rest of the code remains the same
