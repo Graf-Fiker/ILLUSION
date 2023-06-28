bebe1e4512370ca70c48683db8251cedb4209616
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2023-06-28 22:21:34 +0200

    timer

--- assets/js/timer_weekly.js
+++ assets/js/timer_weekly.js
@@ -9,6 +9,11 @@ function updateTimer() {
 
   var timeDiff = target - now; // Calculate the time difference in milliseconds
 
+  // Adjust time difference if negative
+  if (timeDiff < 0) {
+    timeDiff += 7 * 24 * 60 * 60 * 1000; // Add 7 days in milliseconds
+  }
+
   // Calculate days, hours, minutes, and seconds
   var days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
   var hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
