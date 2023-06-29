2629e0ed8e7f4119729b6263a5df3bade588172a
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2023-06-30 00:54:33 +0200

    Update service-worker.js

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -15,3 +15,24 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-pwa-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        // Add other assets you want to cache here
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
