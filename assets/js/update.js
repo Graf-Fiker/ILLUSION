ce93b8f6b236efef4fb2f8f66a6845f26ae07480
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2023-06-29 03:06:37 +0200

    ijk

---
+++ assets/js/update.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Reload to New Version</title>
+</head>
+<body>
+  <button onclick="reloadToNewVersion()">Reload to New Version</button>
+
+  <script>
+    function reloadToNewVersion() {
+      window.location.reload(true);
+    }
+  </script>
+</body>
+</html>
