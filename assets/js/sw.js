87a9ce2b3ac0353936c15a5ab49098be3a412309
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2023-06-30 00:38:36 +0200

    Update sw.js

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -3,11 +3,15 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
       .then(function(registration) {
         // Service Worker registered successfully
-        ;
+        if (navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i)) {
+          console.log('Service Worker registered with scope:', registration.scope);
+        }
       })
       .catch(function(error) {
         // Service Worker registration failed
-        ;
+        if (navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i)) {
+          console.log('Service Worker registration failed:', error);
+        }
       });
   });
 }
