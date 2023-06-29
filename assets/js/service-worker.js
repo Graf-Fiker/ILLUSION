557d29e4a6a7b397b21c62a400f430e248ca41d3
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2023-06-30 01:02:52 +0200

    Update service-worker.js

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -1,4 +1,13 @@
 if ('serviceWorker' in navigator) {
+  const CACHE_NAME = 'my-cache';
+  const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    // Add other static assets you want to cache
+  ];
+
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('service-worker.js')
       .then(function(registration) {
@@ -6,6 +15,9 @@ if ('serviceWorker' in navigator) {
         if (navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i)) {
           console.log('Service Worker registered with scope:', registration.scope);
         }
+
+        // Perform the cache setup
+        registration.waiting?.postMessage({ type: 'SKIP_WAITING' });
       })
       .catch(function(error) {
         // Service Worker registration failed
@@ -14,28 +26,20 @@ if ('serviceWorker' in navigator) {
         }
       });
   });
+
+  self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
 }
-const CACHE_NAME = 'my-cache';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  // Add other static assets you want to cache
-];
-
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
