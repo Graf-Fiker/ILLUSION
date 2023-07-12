140d0a75204e1a7e09372b74e7d42115de9956f0
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2023-07-12 10:10:57 +0200

    a

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -17,6 +17,11 @@ document.addEventListener('DOMContentLoaded', function() {
 		hoverAnimationEl.addEventListener('mouseleave', function(e){ e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate) });
 	});
 
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	  return new bootstrap.Tooltip(tooltipTriggerEl);
+	})
+
 	var toastTriggers = document.querySelectorAll('[data-bs-toggle="toast"]');
 
 	for (let toastTrigger of toastTriggers) {
