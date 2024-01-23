47469b14ab0b76e6f25079749aa5dfd9fcce5ba2
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2024-01-23 18:32:29 +0100

    update

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -16,43 +16,32 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-const cacheName = 'your-app-cache';
+const CACHE_NAME = 'my-cache-v1';
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return fetch('/manifest.json')  // Fetch the manifest to get a list of all resources
-        .then(response => response.json())
-        .then((manifest) => {
-          // Cache all resources listed in the manifest
-          const resourcesToCache = manifest.concat(['/']);  // Add the base URL
-          return cache.addAll(resourcesToCache);
-        });
+    caches.open(CACHE_NAME).then(function(cache) {
+      // Cache the entire website by using a wildcard route
+      return cache.addAll(['/*']);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request).then((response) => {
-        // If the requested resource is not in the cache, add it dynamically
-        return caches.open(cacheName).then((cache) => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then((keyList) => {
+    caches.keys().then(function(cacheNames) {
       return Promise.all(
-        keyList.map((key) => {
-          if (key !== cacheName) {
-            return caches.delete(key);
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
           }
         })
       );
