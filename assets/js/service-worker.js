6b329bf19f8075b8436e363f9b89db41a61fea57
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2024-01-23 19:07:46 +0100

    update

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -16,8 +16,12 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+// service-worker.js
+
 const CACHE_NAME = 'my-cache-v1';
 const OFFLINE_PAGE_URL = '/offline.html';
+
+let isCachingEnabled = true;
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -31,12 +35,32 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
+      if (!isCachingEnabled) {
+        // If caching is disabled, always fetch from the network
+        return fetch(event.request);
+      }
+
       return response || fetch(event.request).catch(function() {
         // If the fetch fails, serve the custom offline page
         return caches.match(OFFLINE_PAGE_URL);
       });
     })
   );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data && event.data.command === 'toggleCaching') {
+    // Handle the toggle caching command
+    isCachingEnabled = !isCachingEnabled;
+    console.log('Caching is ' + (isCachingEnabled ? 'enabled' : 'disabled'));
+
+    // Send the current caching state back to the main page
+    self.clients.matchAll().then(function(clients) {
+      clients.forEach(function(client) {
+        client.postMessage({ command: 'updateButton', cachingEnabled: isCachingEnabled });
+      });
+    });
+  }
 });
 
 self.addEventListener('activate', function(event) {
