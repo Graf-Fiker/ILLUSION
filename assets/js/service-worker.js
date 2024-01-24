414c73c6eb12d17823897f662971965029e93d6c
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2024-01-24 15:02:46 +0100

    Update service-worker.js

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -21,38 +21,6 @@ if ('serviceWorker' in navigator) {
 const CACHE_NAME = 'my-cache-v1';
 const OFFLINE_PAGE_URL = '/offline.html';
 
-let isCachingEnabled = true;
-
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      // Cache the entire website by using a wildcard route
-      return cache.addAll(['/', OFFLINE_PAGE_URL]);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (!isCachingEnabled) {
-        // If caching is disabled, always fetch from the network
-        return fetch(event.request);
-      }
-
-      return response || fetch(event.request).catch(function() {
-        // If the fetch fails, serve the custom offline page
-        return caches.match(OFFLINE_PAGE_URL);
-      });
-    })
-  );
-});
-
-// service-worker.js
-
-const CACHE_NAME = 'my-cache-v1';
-const OFFLINE_PAGE_URL = '/offline.html';
-
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
@@ -67,10 +35,16 @@ self.addEventListener('fetch', function(event) {
     caches.match(event.request).then(function(response) {
       // Serve cached resources or fetch from the network and cache
       return response || fetch(event.request).then(function(fetchResponse) {
-        return caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, fetchResponse.clone());
+        if (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html')) {
+          // Cache the HTML page if it is a navigation request
+          return caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        } else {
+          // For non-HTML requests, just return the fetch response
           return fetchResponse;
-        });
+        }
       });
     }).catch(function() {
       // Serve the offline page for failed requests
