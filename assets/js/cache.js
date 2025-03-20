dcf8c32a5538fa451279667efd0fccdbc427d671
Martin G <127075813+Graf-Fiker@users.noreply.github.com>
2025-03-20 21:27:54 +0100

    pö
    up

--- assets/js/cache.js
+++ assets/js/cache.js
@@ -1,15 +1,22 @@
 self.addEventListener('fetch', (event) => {
-  const requestUrl = new URL(event.request.url);
-
-  if (requestUrl.pathname.endsWith('.jpg') || requestUrl.pathname.endsWith('.png') || requestUrl.pathname.endsWith('.gif')) {
-    // If it's an image, fetch from the network directly
-    event.respondWith(fetch(event.request));
-  } else {
-    // For other resources, use the cache-first strategy
     event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request).catch(() => {
+                return caches.match('/offline.html'); //serve offline page.
+            });
+        })
     );
-  }
+});
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      cache.addAll(urlsToCache);
+      return cache.addAll(['/offline.html']); //cache offline page.
+    })
+  );
 });
